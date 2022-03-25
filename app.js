a3ec21f12e70cb45d0bf9e9798c05ec0d36258de
bavitayadav <bavitayadava008@gmail.com>
2022-03-26 01:46:01 +0530

    upload images issue fixed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var chatRouter = require('./routes/getUsername');
+var fileUploadRouter = require('./routes/fileUploading')	
 
 var app = express();
 
@@ -24,6 +25,8 @@ app.use('/fakepath',express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/chat', chatRouter);
+app.use('/file', fileUploadRouter);
+
 console.log('app.js');
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
